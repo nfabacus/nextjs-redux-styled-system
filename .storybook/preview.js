d465df2d2c0c06c9a3beb8766f9d52a7ddf9b547
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2020-10-13 23:47:20 +0100

    - improves styling structure and uses styled system instead of theme ui

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import { addDecorator } from '@storybook/react';
-//**@jsx jsx */
-import { jsx, ThemeProvider } from 'theme-ui';
+import { ThemeProvider } from 'emotion-theming';
 import theme from '../theme/theme.js';
 
 addDecorator(storyFn => (
